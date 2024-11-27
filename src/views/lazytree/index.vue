30301f287865653e064bd1e24a58a42194ab066b
formosawqd <formosadong@gmail.com>
2024-11-27 17:39:54 +0800

    lazytree

--- src/views/lazytree/index.vue
+++ src/views/lazytree/index.vue
@@ -1,26 +1,24 @@
 <template>
-  <a-spin :spinning="loading">
-    <a-tree
-      :treeData="treeData"
-      :replaceFields="{ children: 'children', title: 'title', key: 'key' }"
-      :loadData="loadData"
-      show-line
-      @expand="expand"
-    >
-      <template slot="plus-square">
-        <a-icon type="plus-square" theme="filled"></a-icon>
-      </template>
-      <template slot="minus-square">
-        <a-icon type="minus-square" theme="filled"></a-icon>
-      </template>
-      <template slot="loading">
-        <a-icon type="loading"></a-icon>
-      </template>
-      <template slot="not">
-        <a-icon type="no"></a-icon>
-      </template>
-    </a-tree>
-  </a-spin>
+  <a-tree
+    :treeData="treeData"
+    :replaceFields="{ children: 'children', title: 'title', key: 'key' }"
+    :loadData="loadData"
+    show-line
+    @expand="expand"
+  >
+    <template slot="plus-square">
+      <a-icon type="plus-square" theme="filled"></a-icon>
+    </template>
+    <template slot="minus-square">
+      <a-icon type="minus-square" theme="filled"></a-icon>
+    </template>
+    <template slot="loading">
+      <a-icon type="loading"></a-icon>
+    </template>
+    <template slot="not">
+      <a-icon type="no"></a-icon>
+    </template>
+  </a-tree>
 </template>
 
 <script>
@@ -32,6 +30,9 @@ export default {
       treeData: [],
       loading: false,
     };
+  },
+  mounted() {
+    this.initTreeData();
   },
   methods: {
     expand(expandedKeys, { expanded, node }) {
@@ -52,13 +53,14 @@ export default {
     async loadData(node) {
       node.dataRef.scopedSlots.switcherIcon = "loading";
       const nodeKey = node.dataRef.key || null; // 根节点传 null
-      this.loading = true;
       try {
         const { data } = await getLazyTree({
           params: { id: nodeKey },
         });
-        if (data.success && data.data.length) {
-          const nodeChildren = data.data.map((item) => ({
+        console.log("data", data);
+
+        if (data.length) {
+          const nodeChildren = data.map((item) => ({
             title: item.label,
             key: item.id,
             isLeaf: item.isLeaf,
@@ -71,19 +73,20 @@ export default {
         } else {
           node.dataRef.scopedSlots.switcherIcon = "not";
         }
+        console.log(this.treeData);
       } catch (error) {
         console.error("加载数据失败", error);
       } finally {
-        this.loading = false;
       }
     },
     // 初始化根节点
     async initTreeData() {
-      this.loading = true;
       try {
         const { data } = await getLazyTree();
-        if (data.success) {
-          this.treeData = data.data.map((item) => ({
+        console.log(data);
+
+        if (data.length) {
+          this.treeData = data.map((item) => ({
             title: item.label,
             key: item.id,
             isLeaf: item.isLeaf,
@@ -91,16 +94,13 @@ export default {
               switcherIcon: "plus-square",
             },
           }));
+          console.log(this.treeData);
         }
       } catch (error) {
         console.error("初始化树数据失败", error);
       } finally {
-        this.loading = false;
       }
     },
-  },
-  mounted() {
-    this.initTreeData();
   },
 };
 </script>
